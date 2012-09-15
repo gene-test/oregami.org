772788a0c3e63b142954ae95207197c8c280e3bc
twendelmuth <thorsten.wendelmuth@gmx.de>
2012-09-15 14:18:13 +0200

    changed to in memory db

--- java/src/test/java/org/oregami/Start.java
+++ java/src/test/java/org/oregami/Start.java
@@ -26,7 +26,7 @@ public class Start {
 		hsqlServer.setSilent(true);
 		hsqlServer.setLogWriter(null);
 		hsqlServer.setDatabaseName(0, "oregami");
-		hsqlServer.setDatabasePath(0, "file:work/oregamidb");
+		hsqlServer.setDatabasePath(0, "mem:work/oregamidb");
 		hsqlServer.setPort(4000);
 
 		hsqlServer.start();
